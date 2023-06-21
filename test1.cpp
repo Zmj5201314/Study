dbf4b9c433a5880e4d506ad00359535cfa6e4fd0
Zmj5201314 <137263896+Zmj5201314@users.noreply.github.com>
2023-06-21 14:54:19 +0800

    Add files via upload

---
+++ test1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "死生契阔" << endl;
+	cout << "与子成说" << endl;
+	cout << "执子之手" << endl;
+	cout << "与子偕老" << endl; 
+	
+	system("pause");
+	
+	return 0;
+}
