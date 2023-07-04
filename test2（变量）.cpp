452721ef7040ecd6b84bb26cae6f5ad56cf76881
Zmj5201314 <137263896+Zmj5201314@users.noreply.github.com>
2023-07-04 14:35:24 +0800

    test2（变量）.cpp

---
+++ test2（变量）.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+ 	
+ 	//±äÁ¿µÄ¶¨Òå 
+	//±äÁ¿´´½¨µÄÓï·¨£ºÊý¾ÝÀàÐÍ ±äÁ¿Ãû = ±äÁ¿³õÊ¼Öµ
+	 
+	 cout << "ÕÔÃô¾ýµÄ³öÉúÈÕÆÚ" << endl;
+	 
+	 //´´½¨±äÁ¿ 
+	int year = 2004;
+	cout << "year = " << year << endl;
+ 	
+ 	int mouth = 8;
+ 	cout << "mouth = " << mouth << endl;
+ 	
+ 	int day = 11;
+ 	cout << "day = " << day << endl;
+ 	
+ 	system("pause");
+ 	
+ 	return 0;
+}
